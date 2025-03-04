db33a7a796b89f7fb16ec643a9f86c1937f1678a
Bobby Thompson <bobbythompson@paypoint.com>
2025-03-04 21:17:10 +0000

    vvv

---
+++ src/Npm.Renovator/Npm.Renovator.RepoReader/Models/PackageJson.cs
@@ -0,0 +1,8 @@
+﻿namespace Npm.Renovator.RepoReader.Models
+{
+    internal record PackageJson
+    {
+        public Dictionary<string, string> DevDependencies { get; init; } = [];
+        public Dictionary<string, string> Dependencies { get; set; } = [];
+    }
+}
