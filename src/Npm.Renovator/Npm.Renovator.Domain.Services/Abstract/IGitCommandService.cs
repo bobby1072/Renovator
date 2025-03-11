a8f5891d9495372158d111a5f952692740a73c98
bobby1072 <bobby.thompson1072@gmail.com>
2025-03-11 23:27:35 +0000

    cff

--- src/Npm.Renovator/Npm.Renovator.Domain.Services/Abstract/IGitCommandService.cs
+++ src/Npm.Renovator/Npm.Renovator.Domain.Services/Abstract/IGitCommandService.cs
@@ -1,6 +1,12 @@
-﻿namespace Npm.Renovator.Domain.Services.Abstract
+﻿using Npm.Renovator.Domain.Models;
+
+namespace Npm.Renovator.Domain.Services.Abstract
 {
     public interface IGitCommandService
     {
+        /// <summary>
+        /// IMPORTANT HTTP(s) ONLY NO SSH OR OTHERS
+        /// </summary>
+        Task<GitCommandResult<Guid>> CheckoutRemoteRepoToLocalTempStoreAsync(Uri remoteRepoLocation, CancellationToken token = default)
     }
 }
