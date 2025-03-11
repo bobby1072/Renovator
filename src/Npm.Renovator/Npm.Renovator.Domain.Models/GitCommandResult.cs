a854ef97299d68089c3124932188be2f666852e6
bobby1072 <bobby.thompson1072@gmail.com>
2025-03-11 23:15:05 +0000

    Starting

--- src/Npm.Renovator/Npm.Renovator.Domain.Models/GitCommandResult.cs
+++ src/Npm.Renovator/Npm.Renovator.Domain.Models/GitCommandResult.cs
@@ -2,6 +2,12 @@
 {
     public record GitCommandResult
     {
-
+        public bool IsSuccess => string.IsNullOrEmpty(ErrorOutput);
+        public string? ErrorOutput { get; set; }
+        public string? StandardOutput { get; set; }
+    }
+    public record GitCommandResult<T> : GitCommandResult
+    {
+        public T? Result { get; set; }
     }
 }
--- src/Npm.Renovator/Npm.Renovator.Domain.Services/Concrete/GitCommandService.cs
+++ src/Npm.Renovator/Npm.Renovator.Domain.Services/Concrete/GitCommandService.cs
@@ -6,6 +6,7 @@ namespace Npm.Renovator.Domain.Services.Concrete
 {
     internal class GitCommandService
     {
+        private const string _tempFolderLocalLocation = "TestGitFolders";
         private readonly ILogger<GitCommandService> _logger;
         public GitCommandService(ILogger<GitCommandService> logger)
         {
@@ -15,9 +16,19 @@ namespace Npm.Renovator.Domain.Services.Concrete
         /// <summary>
         /// HTTP(s) ONLY NO SSH OR OTHERS
         /// </summary>
-        public async Task<GitCommandResult> CheckoutRemoteRepoToLocalTempStoreAsync(Uri remoteRepoLocation, CancellationToken token = default)
+        public async Task<GitCommandResult<Guid>> CheckoutRemoteRepoToLocalTempStoreAsync(Uri remoteRepoLocation, CancellationToken token = default)
         {
             using var ps = PowerShell.Create();
+            var tempRepoId = Guid.NewGuid();
+            
+            ps.AddScript($"git clone {remoteRepoLocation} {Path.Combine(".", _tempFolderLocalLocation, tempRepoId.ToString())}");
+            
+            ps.AddScript($"cd {_tempFolderLocalLocation}");
+            ps.AddScript($"cd {tempRepoId}");
+
+            var result = await ps.InvokeAsync();
+
+            throw new NotImplementedException();
         }
     }
 }
