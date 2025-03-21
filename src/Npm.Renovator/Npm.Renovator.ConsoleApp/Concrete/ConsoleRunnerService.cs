79c7d07f23a910b7bad7f741090d2ac8e8394efb
Bobby Thompson <bobbythompson@paypoint.com>
2025-03-21 21:14:14 +0000

    Fix host

---
+++ src/Npm.Renovator/Npm.Renovator.ConsoleApp/Concrete/ConsoleRunnerService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Hosting;
+using Npm.Renovator.ConsoleApp.Abstract;
+
+namespace Npm.Renovator.ConsoleApp.Concrete
+{
+    public class ConsoleRunnerService: IHostedService
+    {
+        private readonly IConsoleApplicationService _consoleApplicationService;
+        public ConsoleRunnerService(IConsoleApplicationService consoleApplicationService)
+        {
+            _consoleApplicationService = consoleApplicationService;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return _consoleApplicationService.ExecuteAsync();
+        }
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return _consoleApplicationService.DisposeAsync().AsTask();
+        }
+    }
+}
--- src/Npm.Renovator/Npm.Renovator.ConsoleApp/Program.cs
+++ src/Npm.Renovator/Npm.Renovator.ConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Npm.Renovator.ConsoleApp.Abstract;
+using Npm.Renovator.ConsoleApp.Concrete;
 using Npm.Renovator.Domain.Services.Extensions;
 
 namespace Npm.Renovator.ConsoleApp;
@@ -26,19 +27,13 @@ public static class Program
                         {
                             opts.SetMinimumLevel(LogLevel.None);
                         })
-                        .AddTransient<IConsoleApplicationService, Concrete.ConsoleApplicationService>();
+                        .AddTransient<IConsoleApplicationService, Concrete.ConsoleApplicationService>()
+                        .AddHostedService<ConsoleRunnerService>();
                 })
                 .Build();
 
-            await host.StartAsync();
 
-            await using var asyncScope = host.Services.CreateAsyncScope();
-
-            await using var consoleApp = asyncScope.ServiceProvider.GetRequiredService<IConsoleApplicationService>();
-
-            await consoleApp.ExecuteAsync();
-
-            await host.StopAsync();
+            await host.RunAsync();
         }
         catch(System.Exception e)
         {
